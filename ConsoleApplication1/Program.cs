53fdf8d094c0e59c8ca5191d3080f38ce3646814
dake he <keke78ui9@gmail.com>
2014-04-30 11:30:56 -0700

    first check-in, add files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            localhostTestWebServices.WebService1 ws = 
+                new localhostTestWebServices.WebService1();
+            string test = ws.HelloWorld();
+
+
+
+
+            ServiceTest.WebService1SoapClient t
+                = new ServiceTest.WebService1SoapClient();
+            t.Endpoint.Address = new System.ServiceModel.EndpointAddress(
+                "http://localhost:63363/WebService1.asmx");
+            string test1 = t.HelloWorld();
+
+        }
+    }
+}
